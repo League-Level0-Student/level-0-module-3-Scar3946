445ad258140dc3bdde201b0dacc398187b7466d0
root <root@2d03838b9cff>
2020-06-09 00:02:51 +0000

    99bottles

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -9,10 +9,9 @@ public class WalkOfFame {
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-rob.setX(500);
+rob.setX(100); rob.setSpeed(100);
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-rob.penDown(); rob.turn(330);rob.move(270); rob.turn(260); rob.move(300);  
-//rob.turn(65); rob.move(100); rob.turn(180);rob.move(450);
+rob.penDown(); for( int i=0; i<5; i++) {rob.move(200);rob.turn(144);}
 		// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
---
+++ src/_02_loop_variables/_4_99_bottles/bottles/bottles.pde
@@ -0,0 +1,2 @@
+
+for(int i=99;i>0;i--){println("99 bottles of beer 99 bottles of bear take one down pass it around  98 bottles of beer on the wall "); }
